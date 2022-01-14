d1d1444af4348d1281f91764d27f69bf994c4a0d
whodespacito <58792361+whodespacito@users.noreply.github.com>
2022-01-14 08:58:24 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+window.start = function() {
+  let x = 0;
+  let y = 0;
+  document.addEventListener("mousemove", (event) => {
+    x = event.pageX;
+    y = event.pageY;
+  })
+  let Interval = setInterval(() => {
+    var element = document.elementFromPoint(x, y)
+    element.click()
+  })
+}
+window.stop = function() {
+  clearInterval(Interval)
+}
